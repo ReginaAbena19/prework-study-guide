929dcf6184fd52ebc1c9c818e4768dd501100bd4
Regina <reginaabena.tech@hotmail.com>
2023-11-07 11:02:17 +0000

    Added JavaScript to the prework-study-guide application

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+let topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+/*
+if (topics === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topics === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topics === 'Git') {
+  console.log("Let's study Git!");
+} else if (topics === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+*/
+
+for (let i = 0; i < topics.length; i++) {
+    console.log(topics[i]);
+}
